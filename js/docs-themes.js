c4d801e557360609d1bb34f68b7ed51327b6de5a
Dan Coffey <danielpcoffey@gmail.com>
2019-05-02 00:10:00 -0400

    added functionality to pass in manifests dynamically through GET params of dash= and hls=

--- js/docs-themes.js
+++ js/docs-themes.js
@@ -24,3 +24,65 @@ function set_style() {
     switch_style("default");
   }
 }
+
+// split the GET parameters
+function getURLParameter(param) {
+  var pageUrl = window.location.search.substring(1);
+  var urlVars = pageUrl.split('&');
+
+  // iterate over the parameters, and try to match
+  for (var i = 0; i < urlVars.length; i++) {
+    var paramName = urlVars[i].split('=');
+
+    // if match (case insensitive), return it or false
+    if (paramName[0].toLowerCase() == param.toLowerCase()) {
+      return paramName[1];
+    }
+  }
+  return false;
+}
+
+// for when there is a single manifest
+function insertSelectedManifest(kind){
+  if (kind == "hls") {
+    if (getURLParameter("hls")){
+      $("#manifest").val(getURLParameter("hls"));
+      $("#hls-button").trigger( "click" );
+    }
+  }
+
+  if (kind == "dash") {
+    if (getURLParameter("dash")){
+      $("#manifest").val(getURLParameter("dash"));
+      $("#dash-button").trigger( "click" );
+    }
+  }
+}
+
+// if there is a GET parameter for hls or dash, insert them into the input fields
+function insertManifestsFromGet(){
+
+  // hls
+  if (getURLParameter("hls")){
+    $("#hls-manifest").val(getURLParameter("hls"));
+  }
+
+  // dash
+  if (getURLParameter("dash")){
+    $("#dash-manifest").val(getURLParameter("dash"));
+  }
+
+  // insert buttons for custom behavior if only single #manifest
+  if ($("#manifest").length == 1)
+  {
+      if (getURLParameter("hls")) {
+        $(".form-group").append('<button type="button" onclick="insertSelectedManifest(\'hls\')" id="hls-get" class="btn btn-info btn-sm">Load your GET HLS Manifest</button>');
+      }
+
+      if (getURLParameter("dash")){
+        $(".form-group").append('<button type="button" onclick="insertSelectedManifest(\'dash\')" id="dash-get" class="btn btn-info btn-sm">Load your GET DASH Manifest</button>');
+      }
+      
+  }
+    
+};
